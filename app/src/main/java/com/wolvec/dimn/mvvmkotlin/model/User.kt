f3311de669a381058dae1b5230246ab459215838
danie <danielmorenonaranjo@gmail.com>
2020-06-10 21:22:16 -0500

    clase 1=user

--- app/src/main/java/com/wolvec/dimn/mvvmkotlin/model/User.kt
+++ app/src/main/java/com/wolvec/dimn/mvvmkotlin/model/User.kt
@@ -1,8 +1,9 @@
 package com.wolvec.dimn.mvvmkotlin.model
 
 import com.google.gson.annotations.SerializedName
-
+//dataclass es una clase q solo contiene lo mas basico (constructor y getters/setters)
 data class User(
+    //@field:SerializedName("login") se utiliza para serializar el json q recives de una api...en este caso "login" es el nombre del atributo en el json
     @field:SerializedName("login")
     val login: String,
     @field:SerializedName("avatar_url")
