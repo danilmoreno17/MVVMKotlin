66330821535614dad690f095dcf5d86c98242e95
SERVER BIOMETRICO <danielmorenonaranjo@gmail.com>
2020-06-12 16:35:57 -0500

    clase 14 = AppExecutors.kt

---
+++ app/src/main/java/com/wolvec/dimn/mvvmkotlin/AppExecutors.kt
@@ -0,0 +1,43 @@
+package com.wolvec.dimn.mvvmkotlin
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+open class AppExecutors(//clase para ejecutar los hilos
+    val diskIO: Executor,//Hilo de bd local ROOM
+    val networkIO: Executor,//Hilo de la red Retrofit
+    val mainThread: Executor//Hilo principal
+) {
+    @Inject
+    constructor(): this(
+        Executors.newSingleThreadExecutor(),//esta es para diskIO..permite q una vez q se obtenga la instancia executor podemos enviar las tareas
+        Executors.newFixedThreadPool(3),//networkIO...crean 3 hilos q se utilizan ...es decir 3 es el maximo de consultas
+        MainThreadExecutor()//es para nuestro hilo principal
+    )
+
+    fun diskIO(): Executor{
+        return diskIO
+    }
+
+    fun networkIO(): Executor{
+        return networkIO
+    }
+
+    fun mainThread(): Executor{
+        return mainThread
+    }
+
+    private class  MainThreadExecutor: Executor{//hace las tareas en el hilo principal
+        val mainThreadHandler = Handler(Looper.getMainLooper())
+        override fun execute(command: Runnable){
+            mainThreadHandler.post(command)
+        }
+    }
+
+}
