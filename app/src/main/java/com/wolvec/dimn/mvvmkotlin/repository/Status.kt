beab663b8e98d5b54d642aa1f248bafdb968e107
SERVER BIOMETRICO <danielmorenonaranjo@gmail.com>
2020-06-12 16:39:00 -0500

    clase 15 = Status.kt

---
+++ app/src/main/java/com/wolvec/dimn/mvvmkotlin/repository/Status.kt
@@ -0,0 +1,7 @@
+package com.wolvec.dimn.mvvmkotlin.repository
+
+enum class Status {//Enumerador para controlar los estados de un proceso(sea llamada al api o al bd
+    LOADING,
+    SUCCESS,
+    ERROR
+}
